dbf999722bd6df8da6aa69d973e2e39b2cc583be
Marks-1 <onyachmark1@gmail.com>
2022-02-23 06:08:00 +0300

    modified files

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 "use strict";
-// 1. Create two variables, firstCard and secondCard.
-// Set their values to a random number between 2-11
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
 let firstCard = 9;
 let secondCard = 6;
-let sum = firstCard + secondCard + 6;
+let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message;
-// console.log(`The sum of the ${firstCard} and ${secondCard} is ${sum}`);
-
-console.log(message);
-// console.log(hasBlackJack);
-// console.log(isAlive);
 
 let startBtn = document.querySelector("#start-btn");
+let drawBtn = document.querySelector("#new-card");
 let msg = document.querySelector("#message-el");
 
-startBtn.addEventListener("click", function () {
-  if (sum < 21) {
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
+
+startBtn.addEventListener("click", startGame);
+
+function startGame() {
+  cardsEl.textContent = `Cards: ${firstCard} | ${secondCard}`;
+  sumEl.textContent = `Sum: ${sum}`;
+  if (sum <= 21) {
     message = "Do you want to draw another card?";
-    msg.textContent = message;
   } else if (sum === 21) {
     message = "Whoooooh. You have a bkackjack";
-    msg.textContent = message;
     hasBlackJack = true;
   } else {
     message = "You are out of the game";
-    msg.textContent = message;
     isAlive = false;
   }
+  msg.textContent = message;
+}
+// startBtn.addEventListener("click", function () {
+// });
+
+drawBtn.addEventListener("click", function () {
+  let card = 2;
+  sum += card;
+  startGame();
 });
